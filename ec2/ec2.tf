4811b23ffd0e23c9aa5f3e3cac96a617d66f36b2
mahesh <konalamaheshwarareddy@gmail.com>
2024-05-04 18:00:36 +0530

    added ami

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "ec2" {
+  for_each = var.tag
   ami           = var.name
   instance_type = var.instancetype
 
   tags = {
-    for_each = var.tag
     Name = each.value
   }
 }
