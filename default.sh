e6e728e287d7bdb38b33d1f0b95a77a1f2ec8df1
mahesh <konalamaheshwarareddy@gmail.com>
2024-05-05 21:44:50 +0530

    m1

--- default.sh
+++ default.sh
@@ -12,7 +12,7 @@ validate_file() {
     fi
 }
 
-# Set comma as the field separators
+# Set comma as the field separatorsss
 IFS=',' read -ra file_array <<< "${Files[@]}"
 
 # Print each element of the array
